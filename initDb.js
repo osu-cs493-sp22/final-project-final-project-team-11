3be07581172b2c502201f60c4e0f7b96217a3cef
Jack Wright <jwrite333@gmail.com>
2022-06-07 13:59:56 -0700

    Added initialization for courses

--- initDb.js
+++ initDb.js
@@ -5,12 +5,15 @@
 
 const sequelize = require('./lib/sequelize')
 const { UserSchema, UserClientFields } = require('./models/user')
-const { Assignment, AssignmentClientFields } = require('./models/user')
+const { Assignment, AssignmentClientFields } = require('./models/assignment')
+const { Course, CourseClientField } = require('./models/course')
 
 const userData = require('./data/users.json')
 const assignmentData = require('./data/assignments.json')
+const courseData = require('./data/courses.json')
 
 sequelize.sync().then(async function () {
   await UserSchema.bulkCreate(userData, { fields: UserClientFields })
   await Assignment.bulkCreate(assignmentData, { fields: AssignmentClientFields })
+  await Course.bulkCreate(courseData, { fields: CourseClientField })
 })
